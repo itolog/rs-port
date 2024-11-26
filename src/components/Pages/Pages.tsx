31568714deb590f1464aa0a0a501e3f09bcd1fd9
itolog <paukanlove@gmail.com>
2024-11-26 15:32:53 +0200

    base scroll

--- src/components/Pages/Pages.tsx
+++ src/components/Pages/Pages.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+"use client";
+
+import { memo } from "react";
+
+import { pages } from "@/constants";
+import cl from "clsx";
 
 import Home from "@/components/Pages/Home/Home";
 
 import useAppStore from "@/store/appStore";
 import createSelectors from "@/store/createSelectors";
 
+const commonStyle = "w-full h-screen";
+
 const Pages = () => {
   const loading = createSelectors(useAppStore).use.loading();
 
   if (loading) return null;
   return (
-    <div className={"w-screen h-screen absolute"}>
-      <Home />
+    <div className="w-screen flex flex-col items-center justify-center">
+      <section className={cl(pages.HOME, commonStyle)}>
+        <h1>Home</h1>
+        <Home />
+      </section>
+      <section className={cl(pages.SKILLS, commonStyle)}>
+        <h1>SECTION 2</h1>
+      </section>
+      <section className={cl(pages.PROJECTS, commonStyle)}>
+        <h1>SECTION 3</h1>
+      </section>
+      <section className={cl(pages.CONTACT, commonStyle)}>
+        <h1>SECTION 4</h1>
+      </section>
     </div>
   );
 };
 
-export default Pages;
+export default memo(Pages);
