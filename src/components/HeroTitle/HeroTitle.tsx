ebb41197d375164795a8d503432f802cf118d170
itolog <paukanlove@gmail.com>
2024-11-28 12:58:44 +0200

    anim title

--- src/components/HeroTitle/HeroTitle.tsx
+++ src/components/HeroTitle/HeroTitle.tsx
@@ -12,41 +12,12 @@ const commonTextStyle = "flex text-rose-600 font-bold text-3xl";
 
 const HeroTitle = () => {
   const titleClass = cl(commonTextStyle);
-  const descriptionClass = cl(commonTextStyle);
 
   const container = useRef<HTMLDivElement>(null);
   const textRef = useRef<HTMLSpanElement>(null);
 
   useGSAP(() => {
     if (!textRef.current) return;
-    // const tl = gsap.timeline({});
-    //
-    // tl.fromTo(
-    //   ".box",
-    //   { opacity: 0, y: -10 },
-    //   {
-    //     duration: 1,
-    //     opacity: 1,
-    //     y: 0,
-    //     stagger: (index) => {
-    //       return index * 0.1;
-    //     },
-    //   },
-    // );
-
-    // tl.to(".box", {
-    //   duration: 1,
-    //   opacity: (i) => {
-    //     if (i > 0) {
-    //       return 0.1;
-    //     }
-    //     return 1;
-    //   },
-    //   scale: 1,
-    //   stagger: (index) => {
-    //     return index * 0.1;
-    //   },
-    // });
 
     gsap
       .timeline({
@@ -93,24 +64,6 @@ const HeroTitle = () => {
       <span ref={textRef} className={titleClass}>
         {portfolio.title.label}
       </span>
-      {/* <div className={titleClass}> */}
-      {/*   {portfolio.title.label.split("").map((el, index) => { */}
-      {/*     return ( */}
-      {/*       <span className={"box"} key={index}> */}
-      {/*         {el} */}
-      {/*       </span> */}
-      {/*     ); */}
-      {/*   })} */}
-      {/* </div> */}
-      {/* <div className={descriptionClass}> */}
-      {/*   {portfolio.title.description.split("").map((el, index) => { */}
-      {/*     return ( */}
-      {/*       <span className={"box"} key={index}> */}
-      {/*         {el} */}
-      {/*       </span> */}
-      {/*     ); */}
-      {/*   })} */}
-      {/* </div> */}
     </div>
   );
 };
