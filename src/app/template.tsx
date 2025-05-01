e5022a9937107c0ec76a2bc6550c9bf347ca3280
itolog <paukanlove@gmail.com>
2025-05-01 19:20:10 +0300

    fix scrollRestoration

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,6 +3,7 @@
 import { LenisRef, ReactLenis } from "lenis/react";
 import { ReactNode, useEffect, useRef } from "react";
 
+import { portfolio } from "@/config";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
@@ -22,12 +23,20 @@ export default function Template({ children }: { children: ReactNode }) {
     return () => gsap.ticker.remove(update);
   }, []);
 
+  const easing = (x: number): number => {
+    const c1 = 1.70158;
+    const c3 = c1 + 1;
+
+    return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+  };
+
   return (
     <ReactLenis
       root
       options={{
         autoRaf: false,
-        duration: 3,
+        easing,
+        lerp: 1 / portfolio.sections.length,
       }}
       ref={lenisRef}>
       {children}
