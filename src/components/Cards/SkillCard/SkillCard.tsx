2b0180676fbcfeef24cae0bbd92511f40bc0e08c
itolog <paukanlove@gmail.com>
2025-05-02 10:15:27 +0300

    inline styles skill card

--- src/components/Cards/SkillCard/SkillCard.tsx
+++ src/components/Cards/SkillCard/SkillCard.tsx
@@ -5,8 +5,6 @@ import { Skills } from "@/types";
 
 import SvgIcons from "@/components/ui/SvgIcon/SvgIcon";
 
-import styles from "./styles.module.css";
-
 interface RsCardProps {
   skill: Skills;
   classes?: {
@@ -16,13 +14,17 @@ interface RsCardProps {
 
 const SkillCard: FC<RsCardProps> = ({ skill, classes }) => {
   return (
-    <div className={cn(styles.Card, classes?.root)}>
-      <div className={styles.Gradient} />
-      <div className={styles.Info}>
-        <div className={styles.Title}>
-          <SvgIcons name={skill} size={"40px"} />
-        </div>
-        <div className={styles.Text}>{skill}</div>
+    <div
+      className={cn(
+        "relative flex h-[70px] w-60 items-center gap-2 rounded-lg bg-slate-950/60 p-2 shadow-[0_5px_15px_rgba(42,15,15,0.5)] backdrop-blur",
+        "transition duration-200 ease-in hover:bg-slate-950/80",
+        classes?.root,
+      )}>
+      <div className={"text-[2.2em] font-bold"}>
+        <SvgIcons name={skill} size={"40px"} />
+      </div>
+      <div className={"text-[1.2em] font-bold text-[color:var(--primary-color)] capitalize"}>
+        {skill}
       </div>
     </div>
   );
