6252d8dc12fbc95c9d42a04cc870e8a7ae9f640b
itolog <paukanlove@gmail.com>
2025-05-05 14:16:58 +0300

    up depth

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -2,7 +2,7 @@ import { Doto } from "next/font/google";
 
 export const doto = Doto({
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["400", "500", "700", "900"],
   display: "swap",
   fallback: ["system-ui", "arial"],
 });
